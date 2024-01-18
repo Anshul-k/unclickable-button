9caed124b414967703795be552c1af8e21ad1c42
Anshul Kasana <anshul.kasana98@gmail.com>
2024-01-18 21:51:20 +0530

    First Commit

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -29,62 +29,61 @@ const Form = () => {
 
         //send the email using EmailJS
         emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, templateParams, process.env.REACT_APP_PUBLIC_KEY)
-        .then((result) => {
-            console.log(result.text);
-            console.log(selectedDate)
-            setName('');
-            setAge('');
-            setInsta('');
-            setSelectedDate('');
-        }, (error) => {
-            console.log(error.text);
-        });
+            .then((result) => {
+                console.log(result.text);
+                setName('');
+                setAge('');
+                setInsta('');
+                setSelectedDate('');
+            }, (error) => {
+                console.log(error.text);
+            });
 
         //Redirecting to Thanks page
-        navigate("/thanks") 
+        navigate("/thanks")
     };
 
-  return (
-    <section className='formSection'>
-        <div className='formBox'>
-            <div className='square' style={{ '--i': 0 }}></div>
-            <div className='square' style={{ '--i': 1 }}></div>
-            <div className='square' style={{ '--i': 2 }}></div>
-            <div className='square' style={{ '--i': 3 }}></div>
-            <div className='square' style={{ '--i': 4 }}></div>
-            <div className='formContainer'>
-                <div className='form'>
-                    <h2>Let's Select a date !</h2>
-                    <form ref={form} onSubmit={handleSubmit}>
-                        <div className='formInputBox'>    
-                            <input type="text" name="name" placeholder="Name" value={name} required onChange={(e)=> setName(e.target.value)}/>
-                        </div>
-                        <div className='formInputBox'>    
-                            <input type="text" name="age" placeholder="Age" value={age} required onChange={(e)=> setAge(e.target.value)}/>
-                        </div>
-                        <div className='formInputBox'>
-                            <input type="text" name="insta" placeholder="Insta-id" value={insta} required onChange={(e)=> setInsta(e.target.value)}/>
-                        </div>
-                        <div className='formInputBox'>
-                            <DatePicker
-                                selected={selectedDate}
-                                onChange={(date) => setSelectedDate(date)}
-                                placeholderText="Select Date"
-                                dateFormat='dd/MM/yyyy'
-                                minDate={new Date()}
-                                name="date"
-                                required
-                            />
-                        </div>
-                        <div className='formInputBox'>
-                            <input type="submit" value="Send" />
-                        </div>
-                    </form>
+    return (
+        <section className='formSection'>
+            <div className='formBox'>
+                <div className='square' style={{ '--i': 0 }}></div>
+                <div className='square' style={{ '--i': 1 }}></div>
+                <div className='square' style={{ '--i': 2 }}></div>
+                <div className='square' style={{ '--i': 3 }}></div>
+                <div className='square' style={{ '--i': 4 }}></div>
+                <div className='formContainer'>
+                    <div className='form'>
+                        <h2>Let's Select a date !</h2>
+                        <form ref={form} onSubmit={handleSubmit}>
+                            <div className='formInputBox'>
+                                <input type="text" name="name" placeholder="Name" value={name} required onChange={(e) => setName(e.target.value)} />
+                            </div>
+                            <div className='formInputBox'>
+                                <input type="text" name="age" placeholder="Age" value={age} required onChange={(e) => setAge(e.target.value)} />
+                            </div>
+                            <div className='formInputBox'>
+                                <input type="text" name="insta" placeholder="Insta-id" value={insta} required onChange={(e) => setInsta(e.target.value)} />
+                            </div>
+                            <div className='formInputBox'>
+                                <DatePicker
+                                    selected={selectedDate}
+                                    onChange={(date) => setSelectedDate(date)}
+                                    placeholderText="Select Date"
+                                    dateFormat='dd/MM/yyyy'
+                                    minDate={new Date()}
+                                    name="date"
+                                    required
+                                />
+                            </div>
+                            <div className='formInputBox'>
+                                <input type="submit" value="Send" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Form
